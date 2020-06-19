16406fc20362ffbe1bd21fed269fa99461217c6b
kuboshintaro1197 <kuboshintaro1197@DESKTOP-I8L4J6V.ad.se-project.co.jp>
2020-06-19 15:56:36 +0900

    登録画面の追加

--- stringSample/src/main/java/com/example/demo/controller/UserController.java
+++ stringSample/src/main/java/com/example/demo/controller/UserController.java
@@ -5,9 +5,11 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.example.demo.dto.UserRequest;
 import com.example.demo.entity.User;
 import com.example.demo.service.UserService;
 
@@ -17,21 +19,45 @@ import com.example.demo.service.UserService;
 @Controller
 public class UserController {
 
-    /**
-     * ユーザー情報 Service
-     */
-    @Autowired
-    UserService userService;
+	/**
+	 * ユーザー情報 Service
+	 */
+	@Autowired
+	UserService userService;
 
-    /**
-     * ユーザー情報一覧画面を表示
-     * @param model Model
-     * @return ユーザー情報一覧画面のHTML
-     */
-    @RequestMapping(value = "/user/list", method = RequestMethod.GET)
-    public String displayList(Model model) {
-        List<User> userlist = userService.searchAll();
-        model.addAttribute("userlist", userlist);
-        return "user/list";
-    }
+	/**
+	 * ユーザー情報一覧画面を表示
+	 * @param model Model
+	 * @return ユーザー情報一覧画面
+	 */
+	@RequestMapping(value = "/user/list", method = RequestMethod.GET)
+	public String displayList(Model model) {
+		List<User> userlist = userService.searchAll();
+		model.addAttribute("userlist", userlist);
+		return "user/list";
+	}
+
+	/**
+	 * ユーザー新規登録画面を表示
+	 * @param model Model
+	 * @return ユーザー情報一覧画面
+	 */
+	@RequestMapping(value = "/user/add", method = RequestMethod.GET)
+	public String displayAdd(Model model) {
+		model.addAttribute("userRequest", new UserRequest());
+		return "user/add";
+	}
+
+	/**
+	 * ユーザー新規登録
+	 * @param userRequest リクエストデータ
+	 * @param model Model
+	 * @return ユーザー情報一覧画面
+	 */
+	@RequestMapping(value="/user/create", method=RequestMethod.POST)
+    public String create(@ModelAttribute UserRequest userRequest, Model model) {
+		// ユーザー情報の登録
+		userService.create(userRequest);
+		return "redirect:/user/list";
+	}
 }
--- stringSample/src/main/java/com/example/demo/service/UserService.java
+++ stringSample/src/main/java/com/example/demo/service/UserService.java
@@ -1,5 +1,6 @@
 package com.example.demo.service;
 
+import java.util.Date;
 import java.util.List;
 
 import javax.transaction.Transactional;
@@ -7,6 +8,7 @@ import javax.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.example.demo.dto.UserRequest;
 import com.example.demo.entity.User;
 import com.example.demo.repository.UserRepository;
 
@@ -17,14 +19,43 @@ import com.example.demo.repository.UserRepository;
 @Transactional(rollbackOn = Exception.class)
 public class UserService {
 
-    /**
-     * ユーザー情報 Repository
-     */
-    @Autowired
-    UserRepository userRepository;
+	/**
+	 * ユーザー情報 Repository
+	 */
+	@Autowired
+	UserRepository userRepository;
 
-    public List<User> searchAll() {
-        // ユーザーTBLの内容を全検索
-        return userRepository.findAll();
-    }
+	/**
+	 * ユーザー情報 全検索
+	 * @return 検索結果
+	 */
+	public List<User> searchAll() {
+		return userRepository.findAll();
+	}
+
+	/**
+	 * ユーザー情報新規登録
+	 * @param user ユーザー情報
+	 */
+	public void create(UserRequest userRequest) {
+		userRepository.save(CreateUser(userRequest));
+	}
+
+	/**
+	 * ユーザーTBLエンティティの生成
+	 * @param userRequest ユーザー情報リクエストデータ
+	 * @return ユーザーTBLエンティティ
+	 */
+	private User CreateUser(UserRequest userRequest) {
+		Date now = new Date();
+
+		User user = new User();
+		user.setName(userRequest.getName());
+		user.setAddress(userRequest.getAddress());
+		user.setPhone(userRequest.getPhone());
+		user.setCreateDate(now);
+		user.setUpdateDate(now);
+
+		return user;
+	}
 }
