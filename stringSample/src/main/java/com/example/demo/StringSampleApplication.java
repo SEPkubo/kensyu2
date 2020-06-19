a719e3d05a37ae2faa01261aad8ddc8f9b80fe1f
パソコン <@192.168.1.5>
2020-06-19 10:24:28 +0900

    コミット

---
+++ stringSample/src/main/java/com/example/demo/StringSampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StringSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StringSampleApplication.class, args);
+	}
+
+}
