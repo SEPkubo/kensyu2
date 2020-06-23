a976ff404f806e81ad1936cd987befea9738b4a2
パソコン <@192.168.1.5>
2020-06-23 13:38:08 +0900

    登録確認画面追加

--- stringSample/src/main/java/com/example/demo/controller/UserController.java
+++ stringSample/src/main/java/com/example/demo/controller/UserController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -38,15 +39,36 @@ public class UserController {
 	}
 
 	/**
-	 * ユーザー新規登録画面を表示
+	 * 登録画面を表示
 	 * @param model Model
-	 * @return ユーザー情報一覧画面
+	 * @return 一覧画面
 	 */
+
+
 	@RequestMapping(value = "/user/add", method = RequestMethod.GET)
 	public String displayAdd(Model model) {
 		model.addAttribute("userRequest", new UserRequest());
 		return "user/add";
 	}
+
+
+
+//	/**
+//	 * 登録画面確認画面を表示
+//	 * @param model Model
+//	 * @return ユーザー情報一覧画面
+//	 */
+//
+//
+//	@RequestMapping(value = "/user/addcheck", method=RequestMethod.POST)
+//	public String Add(@ModelAttribute UserRequest userRequestadd, Model model) {
+//		System.out.println("通った");
+//		userService.create(userRequestadd);
+//		model.addAttribute("userRequestadd", new UserRequest());
+//		System.out.println(userRequestadd);
+//		return "user/addcheck";
+//	}
+
 
 	/**
 	 * ユーザー新規登録
@@ -58,6 +80,22 @@ public class UserController {
     public String create(@ModelAttribute UserRequest userRequest, Model model) {
 		// ユーザー情報の登録
 		userService.create(userRequest);
+		System.out.println(userRequest);
 		return "redirect:/user/list";
 	}
+
+
+
+//    //  入力画面 登録確認画面は一覧画面とは別に値の受け渡しを行う
+//    @GetMapping("/input")
+//    public String input(@ModelAttribute("userRequestadd") UserRequest userRequestadd) {
+//        return "input";
+//    }
+
+    //  書籍情報画面
+    @PostMapping("/user/addcheck")
+    public String toBookInfo(@ModelAttribute("userRequestadd") UserRequest userRequestadd,Model model) {
+    	model.addAttribute("userRequest", new UserRequest());
+        return "user/addcheck";
+    }
 }
